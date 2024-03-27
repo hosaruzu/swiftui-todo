1354945663a0ad57317359fb64d6314e77dbea02
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-03-27 16:09:40 +0400

    Init xcode project

---
+++ swiftui-todo/swiftui-todo/swiftui_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_todoApp.swift
+//  swiftui-todo
+//
+//  Created by Artem Tebenkov on 27.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
